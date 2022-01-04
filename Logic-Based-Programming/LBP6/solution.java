488b31bdef5ef4f5a92e731e2ee4dfde3432844a
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-01-04 21:36:35 +0530

    Create solution.java

---
+++ Logic-Based-Programming/LBP6/solution.java
@@ -0,0 +1,14 @@
+# Solution:
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner obj=new Scanner(System.in);
+        int n=obj.nextInt();
+        if (n>0)
+            System.out.println(n+n/6);
+    }
+}
