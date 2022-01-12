f95be47e7d4da708bc79880e3bade6396af91e22
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-01-12 16:23:38 +0530

    Create solution2.java

---
+++ Logic-Based-Programming/LBP-013/solution2.java
@@ -0,0 +1,18 @@
+# Using String Function
+
+# Solution:
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner obj=new Scanner(System.in);
+        String s=obj.nextLine();
+        System.out.println(s.length());
+    }
+}
