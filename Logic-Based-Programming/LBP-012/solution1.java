592b60c56c6ee4172db661d3c4fbbe54339cf137
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-01-12 15:26:41 +0530

    Create solution1.java

---
+++ Logic-Based-Programming/LBP-012/solution1.java
@@ -0,0 +1,25 @@
+# Solution:
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner obj=new Scanner(System.in);
+        int n=obj.nextInt();
+        int d,sum=0;
+        if (n>0){
+            while(n!=0){
+                d=n%10;
+                if(d%3==0){
+                    sum=sum+d;
+                }
+                n=n/10;
+            }
+            System.out.println(sum);
+        }
+        else
+            System.exit(0);
+    }
+}
