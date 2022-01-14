adbc56bf8707f5957513ec4a6c2d787c5062985e
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-01-14 15:22:30 +0530

    Update solution2.java

--- Logic-Based-Programming/LBP-019/solution2.java
+++ Logic-Based-Programming/LBP-019/solution2.java
@@ -9,6 +9,11 @@ public class Solution {
 
     public static void main(String[] args) {
         Scanner obj=new Scanner(System.in);
-        System.out.println(Integer.toBinaryString(obj.nextInt()));
+        int n=obj.nextInt();
+        if(n>=0){
+            System.out.println(Integer.toBinaryString(n));
+        }
+        else
+            System.exit(0);
     }
 }
