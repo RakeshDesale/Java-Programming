57e13e6d18a6d5410fff4f0da600b42e53d9424a
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-01-14 15:13:41 +0530

    Create solution2.java

---
+++ Logic-Based-Programming/LBP-019/solution2.java
@@ -0,0 +1,14 @@
+# Using Standard Inbuilt Function
+
+# Solution:
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner obj=new Scanner(System.in);
+        System.out.println(Integer.toBinaryString(obj.nextInt()));
+    }
+}
