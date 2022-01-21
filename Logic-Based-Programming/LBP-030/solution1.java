13146704c168db2a8b3d9483bde2e1061d19fb46
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-01-21 20:09:44 +0530

    Create solution1.java

---
+++ Logic-Based-Programming/LBP-030/solution1.java
@@ -0,0 +1,16 @@
+# Solution:
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner obj=new Scanner(System.in);
+        String str=obj.nextLine();
+        StringBuffer sb=new StringBuffer(str);
+        sb.reverse();
+        
+        System.out.println((str.equals(sb.toString()))?"valid":"invalid");
+    }
+}
