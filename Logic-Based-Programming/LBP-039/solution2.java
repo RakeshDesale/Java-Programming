2ccaa71190dff9d1571ba1e9eff03d3daad4ecc8
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-01-27 19:30:03 +0530

    Create solution2.java

---
+++ Logic-Based-Programming/LBP-039/solution2.java
@@ -0,0 +1,25 @@
+# Solution:
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    static int BlackJack(int n1,  int n2){
+        if (n1>21 && n2>21)
+            return 0;
+        else if (n1==21 && n2==21)
+            return 21;
+        else if (n1>21)
+            return n2;
+        else if (n2>21)
+            return n1;
+        else
+            return (n1>n2)?n1:n2;
+    }
+
+    public static void main(String[] args) {
+        Scanner obj=new Scanner(System.in);
+        int n1=obj.nextInt(), n2=obj.nextInt();
+        System.out.println(BlackJack(n1,n2));
+    }
+}
