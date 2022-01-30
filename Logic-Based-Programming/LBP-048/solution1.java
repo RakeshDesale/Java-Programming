fc0be445193003d1ff9ca507e7a5c8e66f287b9e
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-01-30 14:08:00 +0530

    Create solution1.java

---
+++ Logic-Based-Programming/LBP-048/solution1.java
@@ -0,0 +1,29 @@
+# Solution:
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    static int minToSec(String str){
+        int min,sec;
+        if(str.charAt(0)=='0')
+            min=convert(str.charAt(1));
+        else
+            min=convert(str.charAt(0))*10+convert(str.charAt(1));
+        if(str.charAt(3)=='0')
+            sec=convert(str.charAt(4));
+        else
+            sec=convert(str.charAt(3))*10+convert(str.charAt(4));
+        return min*60+sec;
+    }
+    
+    static int convert(char s){
+        return s-48;
+    }
+
+    public static void main(String[] args) {
+        Scanner obj=new Scanner(System.in);
+        String str=obj.nextLine();
+        System.out.println((minToSec(str)));
+    }
+}
