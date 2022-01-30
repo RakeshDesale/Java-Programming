6a211e50c2b707c31bf8a28149338e3e97324868
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-01-30 16:07:48 +0530

    Update solution5.java

--- Logic-Based-Programming/LBP-048/solution5.java
+++ Logic-Based-Programming/LBP-048/solution5.java
@@ -9,16 +9,16 @@ public class Solution {
 
     public static void main(String[] args) {
         Scanner obj = new Scanner(System.in);
-        String s = obj.nextLine();//ab:cd
-        int n1,n2;
-        if(s.charAt(0)=='0')
-            n1=Integer.valueOf(s.charAt(1)-48);
+        String str = obj.nextLine();
+        int min,sec;
+        if(str.charAt(0)=='0')
+            min=Integer.valueOf(str.charAt(1)-48);
         else
-            n1=Integer.valueOf(s.charAt(0)-48)*10+Integer.valueOf(s.charAt(1)-48);
-        if(s.charAt(3)=='0')
-            n2=Integer.valueOf(s.charAt(4)-48);
+            min=Integer.valueOf(str.charAt(0)-48)*10+Integer.valueOf(str.charAt(1)-48);
+        if(str.charAt(3)=='0')
+            sec=Integer.valueOf(str.charAt(4)-48);
         else
-            n2=Integer.valueOf(s.charAt(3)-48)*10+Integer.valueOf(s.charAt(4)-48);
-        System.out.println(n1*60+n2);
+            sec=Integer.valueOf(str.charAt(3)-48)*10+Integer.valueOf(str.charAt(4)-48);
+        System.out.println(min*60+sec);
     }
 }
