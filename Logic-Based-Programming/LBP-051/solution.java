d3ccfc3f672f2900fa387d0a8583896f2087f37c
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-02-01 13:40:42 +0530

    Create solution.java

---
+++ Logic-Based-Programming/LBP-051/solution.java
@@ -0,0 +1,14 @@
+# Using Predefined replace() method
+
+# Solution:
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner obj=new Scanner(System.in);
+        System.out.println(obj.nextLine().replace(".","[.]"));
+    }
+}
