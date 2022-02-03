5c6aa2e3fda16e3193a14f417e7868430da5ded6
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-02-03 17:24:26 +0530

    Create solution3.java

---
+++ Logic-Based-Programming/LBP-057/solution3.java
@@ -0,0 +1,15 @@
+# Without Using Function
+
+# Solution:
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner obj=new Scanner(System.in);
+        StringBuffer sb=new StringBuffer(obj.nextLine());
+        System.out.println(sb.reverse());
+    }
+}
