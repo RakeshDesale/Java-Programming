17d61295c6edd6d7d558650236c36fac23244053
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-02-03 15:21:45 +0530

    Create solution3.java

---
+++ Logic-Based-Programming/LBP-056/solution3.java
@@ -0,0 +1,15 @@
+# Without Using Function
+
+# Solution:
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner obj=new Scanner(System.in);
+        String str1=obj.nextLine(),str2=obj.nextLine();
+        System.out.println(str2+" "+str1);
+    }
+}
