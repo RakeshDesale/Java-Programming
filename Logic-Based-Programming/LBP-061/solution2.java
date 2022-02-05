29d1bf7f8c176072453b34a10f57f04aa598a485
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-02-05 21:44:08 +0530

    Create solution2.java

---
+++ Logic-Based-Programming/LBP-061/solution2.java
@@ -0,0 +1,19 @@
+# Without Using Function
+
+# Solution:
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    static boolean isChessBoard(String str){
+        int x=str.charAt(0)-96;
+        int y=str.charAt(1);
+        return (x%2!=y%2);
+    }
+
+    public static void main(String[] args) {
+        Scanner obj=new Scanner(System.in);
+        System.out.println(isChessBoard(obj.nextLine()));
+    }
+}
