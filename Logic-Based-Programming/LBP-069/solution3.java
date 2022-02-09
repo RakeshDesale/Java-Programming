91d156a4786b636b28f842064b24a1d109efb23d
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-02-09 18:57:16 +0530

    Create solution3.java

---
+++ Logic-Based-Programming/LBP-069/solution3.java
@@ -0,0 +1,21 @@
+# Another Approach Without Using Function
+
+# Solution:
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        String str = obj.nextLine();
+        int i;
+        for(i=0;i<str.length();i++){
+            if(str.charAt(i)=='a' || str.charAt(i)=='e' || str.charAt(i)=='i' || str.charAt(i)=='o' || str.charAt(i)=='u')
+                continue;
+            else
+                System.out.print(str.charAt(i));
+        }
+    }
+}
