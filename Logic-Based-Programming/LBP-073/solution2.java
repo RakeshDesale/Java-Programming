8512a35f2d83bebbeba34c8672914718044f0a1f
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-02-10 18:30:25 +0530

    Create solution2.java

---
+++ Logic-Based-Programming/LBP-073/solution2.java
@@ -0,0 +1,20 @@
+# Without Using Function
+
+# Solution:
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        String str = obj.nextLine();
+        int i,count=0;
+        for(i=0;i<str.length();i++){
+            if(str.charAt(i)>='0' && str.charAt(i)<='9')
+                count++;
+        }
+        System.out.println(count==5);
+    }
+}
