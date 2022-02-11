a85e6a503247fb01e49046249ddf763dc4d5e4a2
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-02-11 15:53:33 +0530

    Create solution2.java

---
+++ Logic-Based-Programming/LBP-075/solution2.java
@@ -0,0 +1,21 @@
+# Without Using Function
+
+# Solution:
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        String s = obj.nextLine();
+        int i;
+        for(i=0;i<s.length();i++){
+            if(s.charAt(i)=='a' || s.charAt(i)=='e' || s.charAt(i)=='i' || s.charAt(i)=='o' || s.charAt(i)=='u'){
+                System.out.println(i);
+                break;
+            }                
+        }
+    }
+}
