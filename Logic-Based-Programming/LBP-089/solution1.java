0b11d5cdfde79b3e50ca10698a9743827004a6b0
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-02-17 15:42:27 +0530

    Create solution1.java

---
+++ Logic-Based-Programming/LBP-089/solution1.java
@@ -0,0 +1,13 @@
+# Solution:
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        String str1 = obj.nextLine(),str2 = obj.nextLine();
+        System.out.println((str1+str1).contains(str2));
+    }
+}
