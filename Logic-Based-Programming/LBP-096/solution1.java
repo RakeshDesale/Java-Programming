9c8e35adab35ed68d07a905d3a66526d8f0ad9c0
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-02-22 16:45:09 +0530

    Create solution1.java

---
+++ Logic-Based-Programming/LBP-096/solution1.java
@@ -0,0 +1,19 @@
+# Solution:
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        String str = obj.nextLine();
+        int i,count=0;
+        for(i=0;i<str.length();i++){
+            char ch = str.charAt(i);
+            if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u')
+                count++;
+        }
+        System.out.println(count);
+    }
+}
