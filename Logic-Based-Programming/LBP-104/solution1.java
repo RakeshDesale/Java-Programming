d6e9051b00f2cba7985dc9def6f261f6a8b6653b
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-02-28 21:54:47 +0530

    Create solution1.java

---
+++ Logic-Based-Programming/LBP-104/solution1.java
@@ -0,0 +1,20 @@
+# Solution:
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        int n = obj.nextInt(),i,sum=0;
+        int arr[] = new int[n];
+        for(i=0;i<n;i++)
+            arr[i] = obj.nextInt();
+        for(i=0;i<n;i++){
+            if(arr[i]%2!=0)
+                sum = sum+arr[i];
+        }
+        System.out.println(sum);
+    }
+}
