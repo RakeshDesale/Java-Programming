8096692a1ce0b3a60aaf2a8769924ca5e475d872
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-03-01 21:38:31 +0530

    Create solution4.java

---
+++ Logic-Based-Programming/LBP-110/solution4.java
@@ -0,0 +1,33 @@
+# Traditional Approach Using Function
+
+# Solution:
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    static int[] sortArrAsc(int arr[],int n){
+        int i,j,temp=0;
+        for(i=0;i<n;i++){
+            for(j=i+1;j<n;j++){
+                if(arr[i]>arr[j]){
+                    temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        int n = obj.nextInt(),i;
+        int arr[] = new int[n];
+        for(i=0;i<n;i++)
+            arr[i] = obj.nextInt();
+        arr=sortArrAsc(arr,n);
+        for(i=0;i<n;i++)
+            System.out.print(arr[i]+" ");
+    }
+}
