d6e880fdd42d50e6427f505e68d38529d6a7ee3b
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-03-07 22:02:07 +0530

    Create solution7.java

---
+++ Logic-Based-Programming/LBP-114/solution7.java
@@ -0,0 +1,31 @@
+# Traditional Approach (Reverse Approach) Using Function
+
+# Solution:
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    static int minElement(int arr[], int n){
+        int i,j,temp;
+        for(i=0;i<n;i++){
+            for(j=i+1;j<n;j++){
+                if(arr[i]<arr[j]){
+                    temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        return arr[n-1];
+    }
+
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        int n = obj.nextInt(),i;
+        int arr[] = new int[n];
+        for(i=0;i<n;i++)
+            arr[i] = obj.nextInt();
+        System.out.print(minElement(arr,n));
+    }
+}
