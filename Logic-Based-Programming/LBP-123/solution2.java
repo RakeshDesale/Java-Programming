ca6b17b0124176d8cddf1f0969a1353efc99a9bf
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-03-15 17:23:51 +0530

    Create solution2.java

---
+++ Logic-Based-Programming/LBP-123/solution2.java
@@ -0,0 +1,24 @@
+# Using Function
+
+# Solution:
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    static void delEleFromPos(LinkedList ll, int pos){
+        ll.remove(pos);
+        for(Object o:ll.toArray())
+            System.out.print((Integer)o+" ");        
+    }
+
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        int n = obj.nextInt(),i;
+        LinkedList<Integer> ll = new <Integer>LinkedList();
+        for(i=0;i<n;i++)
+            ll.add(obj.nextInt());
+        int pos = obj.nextInt();
+        delEleFromPos(ll,pos);
+    }
+}
