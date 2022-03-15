29ca11201749380f6c74a6de6853c725562101cf
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-03-15 18:03:32 +0530

    Create solution1.java

---
+++ Logic-Based-Programming/LBP-124/solution1.java
@@ -0,0 +1,22 @@
+# Solution:
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        int n = obj.nextInt(),i;
+        LinkedList<Integer> ll = new LinkedList<Integer>();
+        for(i=0;i<n;i++)
+            ll.add(obj.nextInt());
+        int e = obj.nextInt();
+        if(ll.remove(new Integer(e))){
+            for(Object o:ll.toArray())
+                System.out.print((Integer)o+" ");
+        }
+        else
+            System.out.println(-1);
+    }
+}
