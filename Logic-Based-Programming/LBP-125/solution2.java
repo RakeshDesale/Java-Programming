9d8dd835f4f9213534498d8d31511bcb16d6272a
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-03-16 16:44:55 +0530

    Create solution2.java

---
+++ Logic-Based-Programming/LBP-125/solution2.java
@@ -0,0 +1,27 @@
+# Using Function
+
+# Solution:
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    static void updateElement(int arr[], int n, int oldele, int newele){
+        int i;
+        for(i=0;i<n;i++){
+            if(arr[i] == oldele)
+                arr[i] = newele;
+        }
+        for(i=0;i<n;i++)
+            System.out.print(arr[i]+" ");
+    }
+
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        int n = obj.nextInt(),i,arr[] = new int[n];
+        for(i=0;i<n;i++)
+            arr[i] = obj.nextInt();
+        int oldele = obj.nextInt(), newele = obj.nextInt();
+        updateElement(arr,n,oldele,newele);
+    }
+}
