37278be6ec09ec0c7e172d8c4260a90f26813e7e
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-03-17 21:07:53 +0530

    Create solution2.java

---
+++ Logic-Based-Programming/LBP-126/solution2.java
@@ -0,0 +1,26 @@
+# Another Approach
+
+# Solution:
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        int n = obj.nextInt(),arr[] = new int[n],i;
+        for(i=0;i<n;i++)
+            arr[i] = obj.nextInt();
+        int loc = obj.nextInt();
+        int ele = obj.nextInt();
+        for(i=0;i<n;i++){
+            if(i==loc){
+                arr[i] = ele;
+                break;
+            }
+        }
+        for(i=0;i<n;i++)
+            System.out.print(arr[i]+" ");
+    }
+}
