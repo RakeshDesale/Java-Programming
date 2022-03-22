2ac3c5f2334376bb1a18329373e738358409f479
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-03-22 13:13:21 +0530

    Create solution2.java

---
+++ Logic-Based-Programming/LBP-127/solution2.java
@@ -0,0 +1,22 @@
+# Using Function
+
+# Solution:
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    static void arrReverse(int arr[], int n){
+        int i;
+        for(i=n-1;i>=0;i--)
+            System.out.print(arr[i]+" ");
+    }
+
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        int n = obj.nextInt(),i,arr[] = new int[n];
+        for(i=0;i<n;i++)
+            arr[i] = obj.nextInt();
+        arrReverse(arr,n);
+    }
+}
