523225664b751914ca4995a3ae623c79d8515733
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-03-24 20:35:22 +0530

    Create solution1.java

---
+++ Logic-Based-Programming/LBP-131/solution1.java
@@ -0,0 +1,17 @@
+# Solution:
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        int n = obj.nextInt(),i,arr[] = new int[n];
+        for(i=0;i<n;i++)
+            arr[i] = obj.nextInt();
+        Arrays.sort(arr);
+        for(i=0;i<n;i++)
+            System.out.print(arr[i]+" ");
+    }
+}
