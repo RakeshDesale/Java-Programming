25da572fb3771600cdb9660d57dd95e353a257ec
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-03-25 21:21:47 +0530

    Create solution2.java

---
+++ Logic-Based-Programming/LBP-135/solution2.java
@@ -0,0 +1,25 @@
+# Using Function
+
+# Solution:
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    static int oddLocSum(int arr[], int n){
+        int i,sum=0;
+        for(i=0;i<n;i++){
+            if(i%2!=0)
+                sum = sum + arr[i];
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        int n = obj.nextInt(),i,arr[] = new int[n];
+        for(i=0;i<n;i++)
+            arr[i] = obj.nextInt();
+        System.out.println(oddLocSum(arr,n));
+    }
+}
