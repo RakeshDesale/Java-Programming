45299efbe7143104dc70b41d25d544f0a47178a4
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-03-25 20:56:10 +0530

    Create solution1.java

---
+++ Logic-Based-Programming/LBP-133/solution1.java
@@ -0,0 +1,18 @@
+# Solution:
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        int n = obj.nextInt(),i,a[] = new int[n],b[] = new int[n];
+        for(i=0;i<n;i++)
+            a[i] = obj.nextInt();
+        for(i=0;i<n;i++)
+            b[i] = obj.nextInt();
+        for(i=0;i<n;i++)
+            System.out.print((a[i]+b[i])+" ");
+    }
+}
