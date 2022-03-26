27778c92944b7b013dc5d34935c2329906096e92
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-03-26 19:27:55 +0530

    Create solution2.java

---
+++ Logic-Based-Programming/LBP-140/solution2.java
@@ -0,0 +1,24 @@
+# Using Function
+
+# Solution:
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    static void arrDiff(int a[], int b[], int n){
+        int i;
+        for(i=0;i<n;i++)
+            System.out.print((a[i] - b[i])+" ");
+    }
+
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        int n = obj.nextInt(),i,a[] = new int[n],b[] = new int[n];
+        for(i=0;i<n;i++)
+            a[i] = obj.nextInt();
+        for(i=0;i<n;i++)
+            b[i] = obj.nextInt();
+        arrDiff(a,b,n);
+    }
+}
