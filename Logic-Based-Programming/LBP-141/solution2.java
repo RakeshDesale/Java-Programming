9a48c3c496acf53bc67511624ca031d2ea5a5efe
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-03-30 20:47:52 +0530

    Create solution2.java

---
+++ Logic-Based-Programming/LBP-141/solution2.java
@@ -0,0 +1,32 @@
+# Another Approach
+
+# Solution:
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        int n = obj.nextInt(),i,j,temp,low,high,arr[] = new int[n];
+        for(i=0;i<n;i++)
+            arr[i] = obj.nextInt();
+        for(i=0;i<n;i++){
+            for(j=i+1;j<n;j++){
+                if(arr[i]>arr[j]){
+                    temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        low = 0;
+        high = n-1;
+        while(low <= high){
+            System.out.print(arr[low]+" "+arr[high]+" ");
+            low++;
+            high--;
+        }
+    }
+}
