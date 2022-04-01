77c3ac995b1dc2181f72d5287b074af0407fb0d1
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-04-01 13:27:26 +0530

    Create solution1.java

---
+++ Logic-Based-Programming/LBP-145/solution1.java
@@ -0,0 +1,21 @@
+# Solution:
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    static void printEven(int arr[], int n){
+        for(int i=0;i<n;i++){
+            if(arr[i]%2==0)
+                System.out.print(arr[i]+" ");
+        }        
+    }
+
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        int n = obj.nextInt(),i,arr[] = new int[n];
+        for(i=0;i<n;i++)
+            arr[i] = obj.nextInt();
+        printEven(arr,n);
+    }
+}
