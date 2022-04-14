cb5a8005cd9e1ffc33fff0688a831e80f91e11bf
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-04-14 16:19:41 +0530

    Create solution2.java

---
+++ Logic-Based-Programming/LBP-155/solution2.java
@@ -0,0 +1,26 @@
+# Using Core Concept
+
+# Solution:
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        int n = obj.nextInt(), k = obj.nextInt(), arr[] = new int[n], i, j, temp;
+        for(i=0;i<n;i++)
+            arr[i] = obj.nextInt();
+        for(i=0;i<n;i++){
+            for(j=i+1;j<n;j++){
+                if(arr[i]>arr[j]){
+                    temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        System.out.println(arr[k-1]);
+    }
+}
