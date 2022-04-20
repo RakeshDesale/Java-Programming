7d7cfd4099874843a5f0793bf833d808dbfd080b
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-04-20 13:50:58 +0530

    Create solution3.java

---
+++ Logic-Based-Programming/LBP-161/solution3.java
@@ -0,0 +1,30 @@
+# Without Using Function
+
+# Solution:
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        int n1 = obj.nextInt(),i,j=0,count=0,a[] = new int[n1];
+        for(i=0;i<n1;i++)
+            a[i] = obj.nextInt();
+        int n2 = obj.nextInt(),b[] = new int[n2];
+        int c[] = new int[n1+n2];
+        for(i=0;i<n2;i++)
+            b[i] = obj.nextInt();
+        for(i=0;i<n1;i++)
+            c[j++] = a[i];
+        for(i=0;i<n2;i++)
+            c[j++] = b[i];
+        Arrays.sort(c);
+        for(i=0;i<(n1+n2)-1;i++){
+            if(c[i]+1==c[i+1])
+                count++;
+        }
+        System.out.println(count==(n1+n2)-1);
+    }
+}
