6eb4d74c9a38e0a573464feb2b5fd6acb415b05a
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-04-24 16:59:48 +0530

    Create solution2.java

---
+++ Logic-Based-Programming/LBP-169/solution2.java
@@ -0,0 +1,25 @@
+# Using Function
+
+# Solution:
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    static void arrToMatrix(int arr[],int m){
+        int i,j,k=0;
+        for(i=0;i<m;i++){
+            for(j=0;j<m;j++)
+                System.out.print(arr[k++]+" ");
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        int n = obj.nextInt(), m = (int)Math.sqrt(n),i,arr[] = new int[n];
+        for(i=0;i<n;i++)
+            arr[i] = obj.nextInt();
+        arrToMatrix(arr,m);
+    }
+}
