75def92441bee7c2ad79ab64c0f9864648bfb1ea
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-04-26 11:02:51 +0530

    Create solution1.java

---
+++ Logic-Based-Programming/LBP-173/solution1.java
@@ -0,0 +1,12 @@
+# Solution:
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        System.out.println(Integer.valueOf(obj.nextLine(),17));
+    }
+}
