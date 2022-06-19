f0cd07a9feb1d0fe491c508d07e2424701d3f7c3
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-06-19 17:14:29 +0530

    Create solution1.java

---
+++ Logic-Based-Programming/LBP-193/solution1.java
@@ -0,0 +1,20 @@
+# Solution:
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        int n = obj.nextInt();
+        while(true){
+            if(n % 10 == 0){
+                System.out.println(n);
+                break;
+            }
+            else
+                n++;
+        }
+    }
+}
