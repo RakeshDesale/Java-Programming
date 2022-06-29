99028076c47a68d71d533940b95c7e3acd4ff56c
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-06-29 17:57:01 +0530

    Create solution2.java

---
+++ Logic-Based-Programming/LBP-206/solution2.java
@@ -0,0 +1,28 @@
+# Using Function
+
+# Solution:
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    static void matrixRowSum(int arr[][]){
+        int i,j,sum;
+        for(i=0;i<3;i++){
+            sum = 0;
+            for(j=0;j<3;j++)
+                sum = sum + arr[i][j];
+            System.out.println(sum);
+        }        
+    }
+
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        int arr[][] = new int[3][3],i,j;
+        for(i=0;i<3;i++){
+            for(j=0;j<3;j++)
+                arr[i][j] = obj.nextInt();
+        }
+        matrixRowSum(arr);
+    }
+}
