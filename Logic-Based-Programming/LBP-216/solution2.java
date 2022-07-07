2e1e50349aefadb52158b8fc8b68b908792b74f6
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-07-07 18:21:36 +0530

    Create solution2.java

---
+++ Logic-Based-Programming/LBP-216/solution2.java
@@ -0,0 +1,30 @@
+# Using Core Logic
+
+# Solution:
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        int arr[][] = new int[3][3], i, j, k, temp;
+        for(i = 0; i < 3; i++){
+            for(j = 0; j < 3; j++)
+                arr[i][j] = obj.nextInt();
+        }
+        for(i = 0; i < 3; i++){
+            for(j = 0; j < 3; j++){
+                for(k = j + 1; k < 3; k++){
+                    if(arr[i][j] > arr[i][k]){
+                        temp = arr[i][j];
+                        arr[i][j] = arr[i][k];
+                        arr[i][k] = temp;
+                    }
+                }
+            }
+            System.out.println(arr[i][0]);
+        }
+    }
+}
