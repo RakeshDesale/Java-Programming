6504e1eb1b162ee43f6b74844123934cd5f5ec9b
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-07-12 18:05:58 +0530

    Create solution2.java

---
+++ Logic-Based-Programming/LBP-221/solution2.java
@@ -0,0 +1,36 @@
+# Using Function
+
+# Solution:
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    static boolean compareMatrix(int a[][], int b[][]){
+        int i, j;
+        boolean flag = true;
+        for(i = 0; i < 3; i++){
+            for(j = 0; j < 3; j++){
+                if(a[i][j] != b[i][j]){
+                    flag = false;
+                    break;
+                }
+            }
+        }
+        return flag;
+    }
+
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        int a[][] = new int[3][3], b[][] = new int[3][3], i, j;
+        for(i = 0; i < 3; i++){
+            for(j = 0; j < 3; j++)
+                a[i][j] = obj.nextInt();
+        }
+        for(i = 0; i < 3; i++){
+            for(j = 0; j < 3; j++)
+                b[i][j] = obj.nextInt();
+        }
+        System.out.println((compareMatrix(a, b))?"Yes":"No");
+    }
+}
