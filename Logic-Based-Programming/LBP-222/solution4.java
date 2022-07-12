66274262ef2cbfa026889c59c60c2cab3638f347
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-07-12 18:15:22 +0530

    Create solution4.java

---
+++ Logic-Based-Programming/LBP-222/solution4.java
@@ -0,0 +1,36 @@
+# Using Function with Return Value
+
+# Optimized Solution:
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    static int[][] addMatrix(int a[][], int b[][]){
+        int i, j;
+        for(i = 0; i < 3; i++){
+            for(j = 0; j < 3; j++)
+                a[i][j] = a[i][j] + b[i][j];
+        }
+        return a;
+    }
+
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        int a[][] = new int[3][3], b[][] = new int[3][3], i, j;
+        for(i = 0; i < 3; i++){
+            for(j = 0; j < 3; j++)
+                a[i][j] = obj.nextInt();
+        }
+        for(i = 0; i < 3; i++){
+            for(j = 0; j < 3; j++)
+                b[i][j] = obj.nextInt();
+        }
+        a = addMatrix(a, b);
+        for(i =0 ; i < 3; i++){
+            for(j = 0; j < 3; j++)
+                System.out.print(a[i][j] + " ");
+            System.out.println();
+        }
+    }
+}
