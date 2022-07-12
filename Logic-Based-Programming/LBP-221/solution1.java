02078e3d0a1e6aff888850b470feb2ccea8026c5
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-07-12 18:05:14 +0530

    Create solution1.java

---
+++ Logic-Based-Programming/LBP-221/solution1.java
@@ -0,0 +1,30 @@
+# Solution:
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        int a[][] = new int[3][3], b[][] = new int[3][3], i, j;
+        boolean flag = true;
+        for(i = 0; i < 3; i++){
+            for(j = 0;j < 3; j++)
+                a[i][j] = obj.nextInt();
+        }
+        for(i = 0; i < 3; i++){
+            for(j = 0; j < 3; j++)
+                b[i][j] = obj.nextInt();
+        }
+        for(i = 0; i < 3; i++){
+            for(j = 0; j < 3; j++){
+                if(a[i][j] != b[i][j]){
+                    flag = false;
+                    break;
+                }
+            }
+        }
+        System.out.println((flag)?"Yes":"No");
+    }
+}
