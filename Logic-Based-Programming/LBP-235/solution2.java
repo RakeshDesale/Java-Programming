36b88120babec8fc1293172cf869b0a259906511
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-07-20 15:25:05 +0530

    Create solution2.java

---
+++ Logic-Based-Programming/LBP-235/solution2.java
@@ -0,0 +1,31 @@
+# Using Function
+
+# Solution:
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    static boolean isMatUT(int arr[][]){
+        boolean flag = true;
+        for(int i = 0; i < 3; i++){
+            for(int j = 0; j < 3; j++){
+                if(j < i && arr[i][j] != 0){
+                    flag = false;
+                    break;
+                }
+            }
+        }
+        return flag;
+    }
+
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        int arr[][] = new int[3][3], i, j;
+        for(i = 0; i < 3; i++){
+            for(j = 0; j < 3; j++)
+                arr[i][j] = obj.nextInt();
+        }
+        System.out.println(isMatUT(arr) ? "Yes" : "No");
+    }
+}
