ede20461966b7d58cc359b53b667e5bb158fba83
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-07-27 16:27:13 +0530

    Create solution4.java

---
+++ Logic-Based-Programming/LBP-238/solution4.java
@@ -0,0 +1,29 @@
+# Using Function
+
+# Optimized Solution:
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    static boolean symmetricMat(int arr[][]){
+        int i, j, count = 0;
+        for(i = 0; i < 3; i++){
+            for(j = 0; j < 3; j++){
+                if(arr[i][j] == arr[j][i])
+                    count++;
+            }
+        }
+        return count == 9;
+    }
+
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        int arr[][] = new int[3][3], i, j;
+        for(i = 0; i < 3; i++){
+            for(j = 0; j < 3; j++)
+                arr[i][j] = obj.nextInt();
+        }
+        System.out.println(symmetricMat(arr) ? "Yes" : "No");
+    }
+}
