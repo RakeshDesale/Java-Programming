f157f6927a8c35821bbeaa9c8a2d42ae7a2c2b30
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-07-27 16:26:14 +0530

    Create solution2.java

---
+++ Logic-Based-Programming/LBP-238/solution2.java
@@ -0,0 +1,23 @@
+# Optimized Solution:
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        int arr[][] = new int[3][3], i, j, count = 0;
+        for(i = 0; i < 3; i++){
+            for(j = 0; j < 3; j++)
+                arr[i][j] = obj.nextInt();
+        }
+        for(i = 0; i < 3; i++){
+            for(j = 0; j < 3; j++){
+                if(arr[i][j] == arr[j][i])
+                    count++;
+            }
+        }
+        System.out.println((count == 9) ? "Yes" : "No");
+    }
+}
