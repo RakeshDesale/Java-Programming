0a3c78b6a0f1000367900840773495335f721476
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-07-28 20:57:26 +0530

    Create solution1.java

---
+++ Logic-Based-Programming/LBP-239/solution1.java
@@ -0,0 +1,25 @@
+# Solution:
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        int arr[][] = new int[3][3], i, j;
+        for(i = 0; i < 3; i++){
+            for(j = 0; j < 3; j++)
+                arr[i][j] = obj.nextInt();
+        }
+        for(i = 0; i < 3; i++){
+            for(j = 0; j < 3; j++){
+                if(i == j)
+                    System.out.print(arr[i][j] + " ");
+                else
+                    System.out.print("  ");
+            }
+            System.out.println();
+        }
+    }
+}
