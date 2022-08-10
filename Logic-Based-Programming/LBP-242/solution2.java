0ce98bff6a5174a59c40769337a050fd2c188d5c
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-08-10 14:56:29 +0530

    Create solution2.java

---
+++ Logic-Based-Programming/LBP-242/solution2.java
@@ -0,0 +1,25 @@
+# Another Approach
+
+# Solution:
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        int arr[][] = new int[3][3], i, j, sum = 0;
+        for(i = 0; i < 3; i++){
+            for(j = 0; j < 3; j++)
+                arr[i][j] = obj.nextInt();
+        }
+        for(i = 0; i < 3; i++){
+            for(j = 0; j < 3; j++){
+                if(i % 2 != 0)
+                    sum = sum + arr[i][j];
+            }
+        }
+        System.out.println(sum);
+    }
+}
