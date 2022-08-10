204112ab123d2f3b6d693fa1d9a9a12fc587e638
RakeshDesale <87762481+RakeshDesale@users.noreply.github.com>
2022-08-10 14:48:06 +0530

    Create solution4.java

---
+++ Logic-Based-Programming/LBP-241/solution4.java
@@ -0,0 +1,29 @@
+# Another Approach Using Function
+
+# Solution:
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    static int sumOfEvenIndexedRows(int arr[][]){
+        int i, j, sum = 0;
+        for(i = 0; i < 3; i++){
+            for(j = 0; j < 3; j++){
+                if(i % 2 == 0)
+                    sum = sum + arr[i][j];
+            }
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        int arr[][] = new int[3][3], i, j;
+        for(i = 0; i < 3; i++){
+            for(j = 0; j < 3; j++)
+                arr[i][j] = obj.nextInt();
+        }
+        System.out.println(sumOfEvenIndexedRows(arr));
+    }
+}
